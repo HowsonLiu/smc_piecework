3d12c4bf22622ae1e6ad27c90a18051a6c08387b
liuhaosheng <liuhaosheng@rd.netease.com>
2022-01-22 17:14:36 +0800

    + bueaty enter 1 page

--- lib/ui/enter1_page.dart
+++ lib/ui/enter1_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:smc_piecework/manager/artifacts_manager.dart';
+import 'package:smc_piecework/manager/period_manager.dart';
 import 'package:smc_piecework/model/artifacts.dart';
 import 'package:smc_piecework/ui/common/number_input_dialog.dart';
 import 'package:smc_piecework/ui/enter2_page.dart';
+import 'package:smc_piecework/ui/period_page.dart';
 
 class Enter1Page extends StatefulWidget {
   const Enter1Page({Key? key}) : super(key: key);
@@ -28,30 +30,22 @@ class _Enter1PageState extends State<Enter1Page> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("入仓"),
-      ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Text(_artifacts?.name ?? "未设置"),
-            Text(_count != null ? '$_count' : "未设置"),
-            TextButton(
-              child: const Text('选择工件'),
-              onPressed: () => _onArtifactsSelected(context),
-            ),
-            TextButton(
-                onPressed: () => _onCountInput(context),
-                child: const Text('选择工件数量')),
-            TextButton(
-                onPressed: _artifacts != null && _count != null
-                    ? () => _onNextStep(context)
-                    : null,
-                child: const Text('下一步'))
-          ],
+        appBar: AppBar(
+          title: const Text("入仓"),
         ),
-      ),
-    );
+        body: Container(
+          margin: const EdgeInsets.all(40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              _buildPeriodTitle(),
+              _buildArtifactsTitle(),
+              _buildCountTitle(),
+              _buildControlBox(),
+            ],
+          ),
+        ));
   }
 
   Future<void> _onArtifactsSelected(BuildContext context) async {
@@ -122,5 +116,76 @@ class _Enter1PageState extends State<Enter1Page> {
             : Container();
       },
     ));
+  }
+
+  Future<void> _onPeriodSelected(context) async {
+    await Navigator.push(context, MaterialPageRoute(
+      builder: (context) {
+        return const PeriodPage();
+      },
+    ));
+    setState(() {});
+  }
+
+  Widget _buildPeriodTitle() {
+    return Text(
+      PeriodManager.instance.curPeriod?.name ?? '周期设置',
+      style: TextStyle(
+          fontSize: 50,
+          color: (PeriodManager.instance.curPeriod != null
+              ? Colors.black
+              : Colors.grey),
+          fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildArtifactsTitle() {
+    return Text(
+      _artifacts?.name ?? '未选择工件',
+      style: TextStyle(
+          fontSize: 50,
+          color: (_artifacts != null ? Colors.black : Colors.grey),
+          fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildCountTitle() {
+    return Text(
+      _count != null ? '$_count件' : '未设置数量',
+      style: TextStyle(
+          fontSize: 50,
+          color: (_count != null ? Colors.black : Colors.grey),
+          fontWeight: FontWeight.bold),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget _buildControlBox() {
+    return Container(
+        padding: const EdgeInsets.only(left: 200, right: 200),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            PeriodManager.instance.curPeriod == null
+                ? ElevatedButton(
+                    child: const Text('选择周期'),
+                    onPressed: () => _onPeriodSelected(context))
+                : Container(),
+            ElevatedButton(
+              child: const Text('选择工件'),
+              onPressed: () => _onArtifactsSelected(context),
+            ),
+            ElevatedButton(
+                onPressed: () => _onCountInput(context),
+                child: const Text('选择工件数量')),
+            ElevatedButton(
+                onPressed: _artifacts != null && _count != null
+                    ? () => _onNextStep(context)
+                    : null,
+                child: const Text('下一步'))
+          ],
+        ));
   }
 }
