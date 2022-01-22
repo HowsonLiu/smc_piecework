daf7f5fd0ebd073b494a41735c39651ed71152c7
liuhaosheng <liuhaosheng@rd.netease.com>
2022-01-22 15:47:39 +0800

    + bueaty home page

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -32,37 +32,33 @@ class _HomePageState extends State<HomePage> {
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const Enter1Page();
-                  },
-                ));
-              },
-              child: const Text("入仓"),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const StatisticsPage();
-                  },
-                ));
-              },
-              child: const Text("统计"),
-            ),
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (context) {
-                    return const SettingsPage();
-                  },
-                ));
-              },
-              child: const Text("设置"),
-            ),
+            _buildHomeCard(
+                'assets/image/home_page/enter.jpeg', '入仓', Colors.white, () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const Enter1Page();
+                },
+              ));
+            }),
+            _buildHomeCard(
+                'assets/image/home_page/statistics.jpeg', '统计', Colors.black,
+                () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const StatisticsPage();
+                },
+              ));
+            }),
+            _buildHomeCard(
+                'assets/image/home_page/setting.png', '设置', Colors.black, () {
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return const SettingsPage();
+                },
+              ));
+            }),
           ],
         ));
   }
@@ -75,5 +71,42 @@ class _HomePageState extends State<HomePage> {
     ArtifactsManager.instance.fetchFromDatabase();
     PeriodManager.instance.fetchFromLocalStorage();
     JobManager.instance.fetchFromDataBase();
+  }
+
+  Widget _buildHomeCard(String imageUrl, String title, Color? titleColor,
+      void Function()? callback) {
+    return Expanded(
+      child: Card(
+          margin:
+              const EdgeInsets.only(left: 60, right: 60, top: 20, bottom: 20),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(25.0)),
+          clipBehavior: Clip.antiAlias,
+          elevation: 20,
+          child: InkWell(
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.asset(
+                    imageUrl,
+                    fit: BoxFit.cover,
+                  ),
+                  Align(
+                    alignment: FractionalOffset.bottomRight,
+                    child: Container(
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                            fontSize: 50,
+                            color: titleColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      margin: const EdgeInsets.only(right: 20, bottom: 20),
+                    ),
+                  ),
+                ],
+              ),
+              onTap: callback)),
+    );
   }
 }
