7bc3372818222f2b612974d4fac64c0de6803055
liuhaosheng <liuhaosheng@rd.netease.com>
2022-01-23 21:11:18 +0800

    + beauty statistics page

--- lib/ui/statistics_page.dart
+++ lib/ui/statistics_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:smc_piecework/manager/artifacts_manager.dart';
-import 'package:smc_piecework/manager/employee_manager.dart';
 import 'package:smc_piecework/manager/period_manager.dart';
-import 'package:smc_piecework/ui/period_page.dart';
 import 'package:smc_piecework/ui/statistics_employee_page.dart';
 import 'package:smc_piecework/ui/statistics_ticket_page.dart';
 
@@ -15,48 +12,57 @@ class StatisticsPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text("统计"),
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-                flex: 1,
-                child: Text(PeriodManager.instance.curPeriod?.name ?? '未设置')),
-            Card(
-              margin: const EdgeInsets.all(30.0),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0)),
-              child: Column(
-                children: [
-                  ListTile(
-                    leading: const Icon(Icons.people),
-                    title: const Text("按照员工分类"),
-                    trailing: const Icon(Icons.arrow_right),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return StatisticsEmployeePage(
-                            period:
-                                PeriodManager.instance.curPeriod?.name ?? '');
-                      }));
-                    },
+        body: Container(
+            margin: const EdgeInsets.only(left: 50, right: 50, top: 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildPeriodTitle(),
+                Card(
+                  margin: const EdgeInsets.all(30.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.people),
+                        title: const Text("按照员工分类"),
+                        trailing: const Icon(Icons.arrow_right),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return StatisticsEmployeePage(
+                                period:
+                                    PeriodManager.instance.curPeriod?.name ??
+                                        '');
+                          }));
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.list),
+                        title: const Text("按照工单分类"),
+                        trailing: const Icon(Icons.arrow_right),
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return StatisticsTicketPage(
+                                period:
+                                    PeriodManager.instance.curPeriod?.name ??
+                                        '');
+                          }));
+                        },
+                      ),
+                    ],
                   ),
-                  ListTile(
-                    leading: const Icon(Icons.list),
-                    title: const Text("按照工单分类"),
-                    trailing: const Icon(Icons.arrow_right),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return StatisticsTicketPage(
-                            period:
-                                PeriodManager.instance.curPeriod?.name ?? '');
-                      }));
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
-        ));
+                )
+              ],
+            )));
+  }
+
+  _buildPeriodTitle() {
+    return Text(
+      PeriodManager.instance.curPeriod?.name ?? '未设置',
+      style: const TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+    );
   }
 }
